be00b96bfa4083827c8c120f8568ca557da0bb57
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2020-09-26 18:35:33 +0200

    [FEATURE] Add FormFieldValuesPreparer

---
+++ Classes/Domain/Preparers/FormFieldValuesPreparer.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the "jobrouter_base" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Brotkrueml\JobRouterBase\Domain\Preparers;
+
+/**
+ * @internal Only for usage in the TYPO3 JobRouter extensions!
+ */
+final class FormFieldValuesPreparer
+{
+    public function prepareForSubstitution(array $fieldElements, array $fieldsWithValues): array
+    {
+        \array_walk($fieldElements, static function (&$element): void {
+            $element = '';
+        });
+        $fieldsWithValues = \array_merge($fieldElements, $fieldsWithValues);
+
+        $preparedFieldValues = [];
+        foreach ($fieldsWithValues as $name => $value) {
+            $preparedFieldValues[\sprintf('{%s}', $name)]
+                = \is_array($value) ? $this->convertArrayToCsv($value) : $value;
+        }
+
+        return $preparedFieldValues;
+    }
+
+    private function convertArrayToCsv(array $values): string
+    {
+        $fp = \fopen('php://memory', 'r+');
+        if (\fputcsv($fp, $values) === false) {
+            return '';
+        }
+        \rewind($fp);
+
+        return \trim(\stream_get_contents($fp));
+    }
+}
---
+++ Tests/Unit/Domain/Preparers/FormFieldValuesPreparerTest.php
@@ -0,0 +1,93 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the "jobrouter_base" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Domain\Preparers;
+
+use Brotkrueml\JobRouterBase\Domain\Preparers\FormFieldValuesPreparer;
+use PHPUnit\Framework\TestCase;
+use TYPO3\CMS\Form\Domain\Model\FormElements\GenericFormElement;
+
+class FormFieldValuesPreparerTest extends TestCase
+{
+    /** @var FormFieldValuesPreparer */
+    private $subject;
+
+    protected function setUp(): void
+    {
+        $this->subject = new FormFieldValuesPreparer();
+    }
+
+    /**
+     * @test
+     * @dataProvider dataProviderForPrepare
+     */
+    public function prepareForSubstitution(array $fieldElements, array $fieldsWithValues, array $expected): void
+    {
+        $actual = $this->subject->prepareForSubstitution($fieldElements, $fieldsWithValues);
+
+        self::assertSame($expected, $actual);
+    }
+
+    public function dataProviderForPrepare(): \Generator
+    {
+        yield 'No form values given returns form fields with empty strings' => [
+            'fieldElements' => [
+                'foo' => new GenericFormElement('foo', ''),
+                'bar' => new GenericFormElement('bar', ''),
+            ],
+            'fieldsWithValues' => [],
+            'expected' => [
+                '{foo}' => '',
+                '{bar}' => '',
+            ]
+        ];
+
+        yield 'Some form values given returns correct prepared field values' => [
+            'fieldElements' => [
+                'foo' => new GenericFormElement('foo', ''),
+                'bar' => new GenericFormElement('bar', ''),
+            ],
+            'fieldsWithValues' => [
+                'bar' => 'some bar',
+            ],
+            'expected' => [
+                '{foo}' => '',
+                '{bar}' => 'some bar',
+            ]
+        ];
+
+        yield 'All form values given returns correct prepared field values' => [
+            'fieldElements' => [
+                'foo' => new GenericFormElement('foo', ''),
+                'bar' => new GenericFormElement('bar', ''),
+            ],
+            'fieldsWithValues' => [
+                'foo' => 'some foo',
+                'bar' => 'some bar',
+            ],
+            'expected' => [
+                '{foo}' => 'some foo',
+                '{bar}' => 'some bar',
+            ]
+        ];
+
+        yield 'Form value with array (multiple values) returns correct prepared field values' => [
+            'fieldElements' => [
+                'foo' => new GenericFormElement('foo', ''),
+            ],
+            'fieldsWithValues' => [
+                'foo' => ['qux', '"qoo"'],
+            ],
+            'expected' => [
+                '{foo}' => 'qux,"""qoo"""',
+            ]
+        ];
+    }
+}
