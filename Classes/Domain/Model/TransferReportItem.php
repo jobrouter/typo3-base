8774682a19717ede576ecf3664085c92bf586b0e
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-03-06 17:38:35 +0100

    [FEATURE] Add widget for transfer report

---
+++ Classes/Domain/Model/TransferReportItem.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "jobrouter_base" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Brotkrueml\JobRouterBase\Domain\Model;
+
+final class TransferReportItem
+{
+    /**
+     * @var int
+     */
+    private $creationDate;
+
+    /**
+     * @var string
+     */
+    private $message;
+
+    /**
+     * @var string
+     */
+    private $correlationId;
+
+    public function __construct(int $creationDate, string $message, string $correlationId)
+    {
+        $this->creationDate = $creationDate;
+        $this->message = $message;
+        $this->correlationId = $correlationId;
+    }
+
+    public function getCreationDate(): int
+    {
+        return $this->creationDate;
+    }
+
+    public function getMessage(): string
+    {
+        return $this->message;
+    }
+
+    public function getCorrelationId(): string
+    {
+        return $this->correlationId;
+    }
+}
---
+++ Classes/Widgets/TransferReportWidget.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "jobrouter_base" extension for TYPO3 CMS.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+
+namespace Brotkrueml\JobRouterBase\Widgets;
+
+use Brotkrueml\JobRouterBase\Extension;
+use TYPO3\CMS\Dashboard\Widgets\AdditionalCssInterface;
+use TYPO3\CMS\Dashboard\Widgets\ListDataProviderInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetConfigurationInterface;
+use TYPO3\CMS\Dashboard\Widgets\WidgetInterface;
+use TYPO3\CMS\Fluid\View\StandaloneView;
+
+final class TransferReportWidget implements WidgetInterface, AdditionalCssInterface
+{
+    /**
+     * @var WidgetConfigurationInterface
+     */
+    private $configuration;
+
+    /**
+     * @var ListDataProviderInterface
+     */
+    private $dataProvider;
+
+    /**
+     * @var StandaloneView
+     */
+    private $view;
+
+    public function __construct(WidgetConfigurationInterface $configuration, ListDataProviderInterface $dataProvider, StandaloneView $view)
+    {
+        $this->configuration = $configuration;
+        $this->dataProvider = $dataProvider;
+        $this->view = $view;
+    }
+
+    public function renderWidgetContent(): string
+    {
+        $this->view->setTemplate('Widget/TransferReportWidget');
+
+        $this->view->assignMultiple([
+            'configuration' => $this->configuration,
+            'items' => $this->dataProvider->getItems(),
+        ]);
+
+        return $this->view->render();
+    }
+
+    public function getCssFiles(): array
+    {
+        return [
+            \sprintf('EXT:%s/Resources/Public/Css/widgets.css', Extension::KEY),
+        ];
+    }
+}
