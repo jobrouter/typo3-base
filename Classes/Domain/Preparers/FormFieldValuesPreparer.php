2243537793e9e0b02a1718b3edc9761a82ddfe7a
Chris Müller <2566282+brotkrueml@users.noreply.github.com>
2021-10-25 18:01:24 +0200

    [TASK] Fix phpstan errors for FormFieldValuesPreparer

--- Classes/Domain/Preparers/FormFieldValuesPreparer.php
+++ Classes/Domain/Preparers/FormFieldValuesPreparer.php
@@ -11,12 +11,16 @@ declare(strict_types=1);
 
 namespace Brotkrueml\JobRouterBase\Domain\Preparers;
 
+use TYPO3\CMS\Form\Domain\Model\FormElements\FormElementInterface;
+
 /**
  * @internal Only for usage in the TYPO3 JobRouter extensions!
  */
 final class FormFieldValuesPreparer
 {
     /**
+     * @param array<string, FormElementInterface> $fieldElements
+     * @param array<string, string|list<string>> $fieldsWithValues
      * @return array<string, mixed>
      */
     public function prepareForSubstitution(array $fieldElements, array $fieldsWithValues): array
@@ -35,14 +39,25 @@ final class FormFieldValuesPreparer
         return $preparedFieldValues;
     }
 
+    /**
+     * @param list<string> $values
+     */
     private function convertArrayToCsv(array $values): string
     {
         $fp = \fopen('php://memory', 'r+');
+        if ($fp === false) {
+            throw new \RuntimeException('Error opening php://memory', 1635177287);
+        }
         if (\fputcsv($fp, $values) === false) {
             return '';
         }
         \rewind($fp);
 
-        return \trim(\stream_get_contents($fp));
+        $csv = \stream_get_contents($fp);
+        if ($csv === false) {
+            throw new \RuntimeException('Cannot retrieve CSV content', 1635177288);
+        }
+
+        return \trim($csv);
     }
 }
